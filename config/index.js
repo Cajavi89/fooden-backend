873a84c5a5c347d5e4bb51c81d2b5115f02a41c7
Carlos <cajavi89@gmail.com>
2022-02-24 16:34:50 -0500

    fix

--- config/index.js
+++ config/index.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 
 const all= {
   env: process.env.NODE_ENV,
-  port: process.env.PORT || 8080,
+  port: process.env.PORT,
   expiresInL: '24h',
   seedDB: process.env.NODE_ENV !== 'production',
   secrets: {
     session: process.env.SECRET_KEY || 'th1s_s3cr3t_k3y'
   },
   userRoles:['admin', 'user'],
-  googleKey: process.env.GOOGLE_API_KEY
 };
 
 module.exports = all;
--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const users = require('./api/user');
 const uploads = require('./api/uploads');
 
 function routes(app) {
-  app.use('/api/fooden', fooden) ;
+  app.use('/', fooden) ;
   app.use('/api/restaurants', restaurants);
   app.use('/api/users', users);
   app.use('/api/uploads', uploads);
